2ccc05ffb676757f7d848364525f23e5da48a131
Andreas <704542+tullo@users.noreply.github.com>
2023-02-05 17:20:38 +0000

    Delete run.sh

--- cmd/otconf/run.sh
+++
@@ -1,8 +0,0 @@
-# export $(grep -v '^#' .env | xargs)
-# export $(grep -v '^#' .env)
-
-# Run process in spec environment
-# env $(cat .env  | xargs) | grep LS_
-# env $(cat .env) | grep LS_
-go build
-env $(cat .env | grep -v '^#') ./otconf
